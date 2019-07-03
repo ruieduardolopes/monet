d435cde6a4a5bfb58c142dd658a6f2ac654c5f51
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-03 16:39:20 +0100

    Add André as author and change the CLI options

--- src/options.rs
+++ src/options.rs
@@ -3,14 +3,27 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 pub fn get_options_from_cli() -> ArgMatches<'static> {
     App::new("Monet: Monitor a Network")
         .version("0.1.0")
-        .author("Rui Lopes")
+        .author("André Nascimento and Rui Lopes")
         .about("Tool to get some insights on a network or interfaces")
         .subcommand(
-            SubCommand::with_name("perf")
-                .about("Run a network performance test.")
-                .subcommand(SubCommand::with_name("server").about("Run an instance as a server."))
-                .subcommand(SubCommand::with_name("client").about("Run an instance as a client.")),
+            SubCommand::with_name("profile")
+                .arg(
+                    Arg::with_name("ingress-interfaces")
+                        .takes_value(true)
+                        .long("ingress-interfaces")
+                        .short("I")
+                        .required(true)
+                        .help("Set of ingress interfaces, between quotes: i.e. \"enp0s1,wlan0\"")
+                )
+                .arg(
+                    Arg::with_name("egress-interfaces")
+                        .takes_value(true)
+                        .long("egress-interfaces")
+                        .short("E")
+                        .required(true)
+                        .help("Set of egress interfaces, between quotes: i.e. \"enp0s1,wlan0\"")
+                )
+                .about("Capture and analyze traffic passing through a node, passing from an ingress interface to an egress one.")
         )
-        .subcommand(SubCommand::with_name("interface").about("Analyze"))
         .get_matches()
 }
