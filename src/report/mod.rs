bcc6725de9ed78f499d5be8de6e73c5bb05f84e9
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-05 15:36:39 +0100

    Add report initialization function in report module

--- src/report/mod.rs
+++ src/report/mod.rs
@@ -1,1 +1,25 @@
-pub fn init()
+use slog::{o, Drain, Logger};
+use std::fs::{OpenOptions, File};
+
+pub fn init(ingress: bool, interface: String) -> Logger {
+    let log_file: File = if ingress {
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&format!("ingress-{}.log", interface))
+            .unwrap()
+    } else {
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&format!("egress-{}.log", interface))
+            .unwrap()
+    };
+
+    let file_decorator = slog_term::PlainDecorator::new(log_file);
+    let file_drain = slog_term::FullFormat::new(file_decorator).build().fuse();
+    let file_drain = slog_async::Async::new(file_drain).build().fuse();
+    slog::Logger::root(file_drain, o!())
+}
