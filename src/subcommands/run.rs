e8ab373875674536b4611a4d8339be2e31680292
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-24 15:59:16 +0100

    Remove .monet directory and create a new one per execution

--- src/subcommands/run.rs
+++ src/subcommands/run.rs
@@ -5,38 +5,17 @@ use regex::Regex;
 use std::collections::BTreeMap;
 use std::fs::{File, OpenOptions};
 use std::io::Error;
+use std::path::PathBuf;
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 use std::{env, process};
-use std::path::PathBuf;
 
 use crate::capture::interfaces::*;
 use crate::capture::results::CaptureResult;
 use crate::structs::node::Node;
-
-//fn start_captures(
-//    scope: &Scope,
-//    session: &Arc<AtomicUsize>,
-//    captures: &'static mut BTreeMap<String, Vec<CaptureResult>>,
-//    interfaces: &'static Vec<String>,
-//    is_ingress: bool,
-//    filter: &'static Regex,
-//) {
-//    interfaces.into_iter().for_each(|interface| {
-//        captures.insert(
-//            format!(
-//                "{}-{}",
-//                if is_ingress { "ingress" } else { "egress" },
-//                interface
-//            ),
-//            run_capture(true, interface, filter).unwrap(),
-//        );
-//    });
-//}
+use crate::structs::node::status::NodeStatus;
 
 pub fn init(ingress: String, egress: String, filter: Regex) -> Result<(), Error> {
-    println!("Initializing");
-
     // Start both ingress and egress captures.
     thread::scope(|scope| {
         // Override instructions for handling Ctrl-C signal.
@@ -45,12 +24,20 @@ pub fn init(ingress: String, egress: String, filter: Regex) -> Result<(), Error>
         signal_hook::flag::register_usize(signal_hook::SIGINT, Arc::clone(&session), SIGINT);
 
         let home = match env::var_os("HOME") {
-            None => { println!("$HOME not set."); process::exit(1) }
+            None => {
+                panic!("$HOME not set.");
+            }
             Some(path) => PathBuf::from(path),
         };
+        std::fs::remove_dir_all(format!("{}/.monet/", home.display())).unwrap();
+        std::fs::create_dir(format!("{}/.monet/", home.display())).unwrap();
 
         // Set this execution as a node with ingress and egress interfaces.
         let node = Node::new(ingress, egress);
+        match node.status() {
+            NodeStatus::NotANode => panic!("Quitting, since no interfaces were given as both ingress and egress."),
+            _ => println!("Running monet on a {}...", node.status()),
+        }
 
         // Start captures for ingress interfaces.
         node.all().par_iter().for_each(|(interface, is_ingress)| {
@@ -68,10 +55,19 @@ pub fn init(ingress: String, egress: String, filter: Regex) -> Result<(), Error>
                 .create_new(true)
                 .write(true)
                 .open(if *is_ingress {
-                    println!("Trying to create {}", format!("{}/.monet/ingress-{}.monet", home.display(), interface)); format!("{}/.monet/ingress-{}.monet", home.display(), interface)
+                    println!(
+                        "\nTrying to create {}",
+                        format!("{}/.monet/ingress-{}.monet", home.display(), interface)
+                    );
+                    format!("{}/.monet/ingress-{}.monet", home.display(), interface)
                 } else {
-                    println!("Trying to create {}", format!("{}/.monet/egress-{}.monet", home.display(), interface)); format!("{}/.monet/egress-{}.monet", home.display(), interface)
-                }).unwrap();
+                    println!(
+                        "\nTrying to create {}",
+                        format!("{}/.monet/egress-{}.monet", home.display(), interface)
+                    );
+                    format!("{}/.monet/egress-{}.monet", home.display(), interface)
+                })
+                .unwrap();
             bincode::serialize_into(&mut file, &capture).unwrap();
         });
     });
