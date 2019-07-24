f7ede00e17874e61a5920661c3ee3106240e3c72
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-24 15:58:48 +0100

    Add status to Node struct

--- src/structs/node/mod.rs
+++ src/structs/node/mod.rs
@@ -1,7 +1,12 @@
+pub mod status;
+
+use crate::structs::node::status::NodeStatus;
+
 #[derive(Clone, Debug)]
 pub struct Node {
     ingress_interfaces: Vec<String>,
     egress_interfaces: Vec<String>,
+    status: NodeStatus,
 }
 
 impl Node {
@@ -22,15 +27,34 @@ impl Node {
             .filter(|element| element.len() > 0)
             .collect();
 
+        // State the status of the current node.
+        let status: NodeStatus;
+        if ingress_interfaces.len() == 0 {
+            if egress_interfaces.len() == 0 {
+                status = NodeStatus::NotANode;
+            } else {
+                status = NodeStatus::StartingNode;
+            }
+        } else if egress_interfaces.len() == 0 {
+            if ingress_interfaces.len() == 0 {
+                status = NodeStatus::NotANode;
+            } else {
+                status = NodeStatus::EndingNode;
+            }
+        } else {
+            status = NodeStatus::IntermediateNode;
+        }
+
         // Create the structure as is with the given data.
         Self {
             ingress_interfaces,
             egress_interfaces,
+            status,
         }
     }
 
     pub fn status(&self) -> NodeStatus {
-        
+        self.status
     }
 
     pub fn ingress(&self) -> Vec<String> {
