ea0b6f275e5ca45f9ca5419c9c4aab9eda88bfd5
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-24 15:58:32 +0100

    Implement block strategy to reporter module

--- src/report/mod.rs
+++ src/report/mod.rs
@@ -1,5 +1,9 @@
-use slog::{o, Drain, Logger};
+use slog::{info, o, Drain, Logger};
 use std::fs::{File, OpenOptions};
+use std::io::Error;
+
+use crate::capture::results::CaptureResult;
+use slog_async::OverflowStrategy;
 
 pub fn init(ingress: bool, interface: String) -> Logger {
     let log_file: File = if ingress {
@@ -20,6 +24,14 @@ pub fn init(ingress: bool, interface: String) -> Logger {
 
     let file_decorator = slog_term::PlainDecorator::new(log_file);
     let file_drain = slog_term::FullFormat::new(file_decorator).build().fuse();
-    let file_drain = slog_async::Async::new(file_drain).build().fuse();
+    let file_drain = slog_async::Async::new(file_drain).overflow_strategy(OverflowStrategy::Block).build().fuse();
     slog::Logger::root(file_drain, o!())
+}
+
+pub fn report(reporter: &Logger, records: &Vec<CaptureResult>) -> Result<(), Error> {
+    records
+        .iter()
+        .for_each(|record| info!(reporter, "{}", record));
+
+    Ok(())
 }
