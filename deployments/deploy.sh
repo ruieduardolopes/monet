2edca8950ed15100b4ae43fde59b12c89ce3a24f
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2019-07-24 17:56:07 +0100

    Update travis YAML and deployments/deploy.sh script

---
+++ deployments/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # TODO Update this to build the artifacts that matter to you
+    cross rustc --bin monet --target $TARGET --release -- -C lto
+
+    # TODO Update this to package the right artifacts
+    cp target/$TARGET/release/monet $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
